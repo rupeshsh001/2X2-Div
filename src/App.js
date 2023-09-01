60c6146f54eb46483c361c449983c7a75c60c44f
rupesh.s <rupesh.s@valuebound.com>
2023-09-01 14:28:25 +0530

    create the 2*2 layout

--- src/App.js
+++ src/App.js
@@ -1,49 +1,4 @@
-// import React from 'react';
-// import './App.css'; // Assuming you have a CSS file to apply the styles
-
-// function App() {
-//     return (
-//         <>
-//             <div className='wrapper'>
-//                 <div className='top-left'></div>
-//                 <div className='top-right'></div>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default App;
-
-// import React, { useState } from 'react';
-// import './App.css';
-
-// function App() {
-//     const [div1Width, setDiv1Width] = useState('50%');
-//     const [div1Height, setDiv1Height] = useState('50%');
-
-//     const handleResize = (newWidth) => {
-//         const newDiv1Width = `${newWidth}%`;
-//         const newDiv2Width = `${100 - newWidth}%`;
-//         setDiv1Width(newDiv1Width);
-//         setDiv1Height(newDiv1Width);
-//     };
-
-//     return (
-//         <div className='grid-container'>
-//             <div className='resizable' style={{ width: div1Width, height: div1Height }}>
-//                 <div className='resize-handle' onMouseUp={() => handleResize(70)}></div>
-//                 {/* Content for the first resizable div */}
-//             </div>
-//             <div className='resizable'>{/* Content for the second resizable div */}</div>
-//             <div className='resizable'>{/* Content for the third resizable div */}</div>
-//             <div className='resizable'>{/* Content for the fourth resizable div */}</div>
-//         </div>
-//     );
-// }
-
-// export default App;
-
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './Layout';
 
 const App = () => {
@@ -53,64 +8,18 @@ const App = () => {
         cell3: 'cell 3',
         cell4: 'cell 4',
     };
+
+    const [inputs, setInputs] = useState({
+        input1: { isEditing: false, text: 'Cell 1' },
+        input2: { isEditing: false, text: 'Cell 2' },
+        input3: { isEditing: false, text: 'Cell 3' },
+        input4: { isEditing: false, text: 'Cell 4' },
+    });
     return (
         <div style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '100vh', alignItems: 'center' }}>
-            <Layout width={500} height={500} data={divData} />
+            <Layout width={500} height={500} data={divData} inputs={inputs} setInputs={setInputs} />
         </div>
     );
 };
 
 export default App;
-
-// import React, { useState } from 'react';
-// import './App.css';
-
-// const App = () => {
-//     const [pointerPosition, setPointerPosition] = useState({ x: 0, y: 0 });
-
-//     const handleMouseMove = (event) => {
-//         const rect = event.target.getBoundingClientRect();
-//         const x = event.clientX - rect.left;
-//         const y = event.clientY - rect.top;
-//         setPointerPosition({ x, y });
-//     };
-
-//     return (
-//         <div className='outer-rectangle' onMouseMove={handleMouseMove}>
-//             <div
-//                 className='inner-rectangle'
-//                 style={{ left: pointerPosition.x, top: pointerPosition.y, backgroundColor: 'pink' }}
-//             ></div>
-//             <div
-//                 className='inner-rectangle'
-//                 style={{ left: pointerPosition.x, top: pointerPosition.y, backgroundColor: 'pink' }}
-//             ></div>
-//             <div
-//                 className='inner-rectangle'
-//                 style={{ left: pointerPosition.x, top: pointerPosition.y, backgroundColor: 'pink' }}
-//             ></div>
-//             <div
-//                 className='inner-rectangle'
-//                 style={{ left: pointerPosition.x, top: pointerPosition.y, backgroundColor: 'pink' }}
-//             ></div>
-//         </div>
-//     );
-// };
-
-// export default App;
-
-// import React from 'react';
-// import './App.css'; // Assuming you have a CSS file to apply the styles
-
-// function App() {
-//     return (
-//         <div className='wrapper'>
-//             <div className='top-left'></div>
-//             <div className='top-right'></div>
-//             <div className='bottom-left'></div>
-//             <div className='bottom-right'></div>
-//         </div>
-//     );
-// }
-
-// export default App;
--- src/Layout.js
+++ src/Layout.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import Draggable from 'react-draggable';
 import './App.css';
 
-function Layout({ width, height, data }) {
+function Layout({ width, height, data, inputs, setInputs }) {
     const myref = useRef();
     const [maxX, setMaxX] = useState(width);
     const [maxY, setMaxY] = useState(height);
@@ -39,67 +39,179 @@ function Layout({ width, height, data }) {
         if (myref?.current?.offsetTop) setMaxY(myref?.current?.offsetTop + height);
     }, []);
 
+    const handleEditClick = (inputKey) => {
+        setInputs((prevInputs) => ({
+            ...prevInputs,
+            [inputKey]: { ...prevInputs[inputKey], isEditing: true },
+        }));
+    };
+
+    const handleBlur = (inputKey) => {
+        setInputs((prevInputs) => ({
+            ...prevInputs,
+            [inputKey]: { ...prevInputs[inputKey], isEditing: false },
+        }));
+    };
+
+    const handleChange = (e, inputKey) => {
+        setInputs((prevInputs) => ({
+            ...prevInputs,
+            [inputKey]: { ...prevInputs[inputKey], text: e.target.value },
+        }));
+    };
+
     return (
-        <div style={{ position: 'relative', width: width, height: height }} ref={myref}>
-            <div style={{ display: 'flex' }}>
-                <div style={{ width: initialState.x, height: initialState.y, backgroundColor: 'yellow', ...alignCenter }}>
-                    {data.cell1}
+        <>
+            <div style={{ position: 'relative', width: width, height: height }} ref={myref}>
+                <div style={{ display: 'flex' }}>
+                    <div
+                        style={{ width: initialState.x, height: initialState.y, backgroundColor: 'yellow', ...alignCenter }}
+                    >
+                        {inputs.input1.isEditing ? (
+                            <input
+                                type='text'
+                                value={inputs.input1.text}
+                                onChange={(e) => handleChange(e, 'input1')}
+                                onBlur={() => handleBlur('input1')}
+                                autoFocus
+                            />
+                        ) : (
+                            <span onClick={() => handleEditClick('input1')}>{inputs.input1.text}</span>
+                        )}
+                    </div>
+                    <div
+                        style={{
+                            width: Math.abs(width - initialState.x),
+                            height: initialState.y,
+                            backgroundColor: 'pink',
+                            ...alignCenter,
+                        }}
+                    >
+                        {inputs.input2.isEditing ? (
+                            <input
+                                type='text'
+                                value={inputs.input2.text}
+                                onChange={(e) => handleChange(e, 'input2')}
+                                onBlur={() => handleBlur('input2')}
+                                autoFocus
+                            />
+                        ) : (
+                            <span onClick={() => handleEditClick('input2')}>{inputs.input2.text}</span>
+                        )}
+                    </div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                    <div
+                        style={{
+                            width: initialState.x,
+                            height: Math.abs(height - initialState.y),
+                            backgroundColor: 'red',
+                            ...alignCenter,
+                        }}
+                    >
+                        {inputs.input3.isEditing ? (
+                            <input
+                                type='text'
+                                value={inputs.input3.text}
+                                onChange={(e) => handleChange(e, 'input3')}
+                                onBlur={() => handleBlur('input3')}
+                                autoFocus
+                            />
+                        ) : (
+                            <span onClick={() => handleEditClick('input3')}>{inputs.input3.text}</span>
+                        )}
+                    </div>
+                    <div
+                        style={{
+                            width: Math.abs(initialState.x - width),
+                            height: Math.abs(initialState.y - height),
+                            backgroundColor: 'blue',
+                            ...alignCenter,
+                        }}
+                    >
+                        {inputs.input4.isEditing ? (
+                            <input
+                                type='text'
+                                value={inputs.input4.text}
+                                onChange={(e) => handleChange(e, 'input4')}
+                                onBlur={() => handleBlur('input4')}
+                                autoFocus
+                            />
+                        ) : (
+                            <span onClick={() => handleEditClick('input4')}>{inputs.input4.text}</span>
+                        )}
+                    </div>
                 </div>
                 <div
                     style={{
-                        width: Math.abs(width - initialState.x),
-                        height: initialState.y,
-                        backgroundColor: 'pink',
-                        ...alignCenter,
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
                     }}
                 >
-                    {data.cell2}
-                </div>
-            </div>
-            <div style={{ display: 'flex' }}>
-                <div
-                    style={{
-                        width: initialState.x,
-                        height: Math.abs(height - initialState.y),
-                        backgroundColor: 'red',
-                        ...alignCenter,
-                    }}
-                >
-                    {data.cell3}
-                </div>
-                <div
-                    style={{
-                        width: Math.abs(initialState.x - width),
-                        height: Math.abs(initialState.y - height),
-                        backgroundColor: 'blue',
-                        ...alignCenter,
-                    }}
-                >
-                    {data.cell4}
+                    <Draggable
+                        onDrag={handleDrag}
+                        bounds={{
+                            top: -height / 2,
+                            left: -width / 2,
+                            right: width / 2,
+                            bottom: height / 2,
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                width: '40px',
+                                height: '40px',
+                                ...plusStyle,
+                            }}
+                        >
+                            +
+                        </div>
+                    </Draggable>
                 </div>
             </div>
             <div
                 style={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%,-50%) ',
-                    ...plusStyle,
+                    backgroundColor: '#ffffff',
+                    borderRadius: '8px',
+                    boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.5)',
+                    padding: '20px',
+                    margin: '20px',
+                    display: 'inline-block',
+                    width: '250px',
                 }}
             >
-                <Draggable
-                    onDrag={handleDrag}
-                    bounds={{
-                        top: -height / 2,
-                        left: -width / 2,
-                        right: width / 2,
-                        bottom: height / 2,
-                    }}
-                >
-                    <div>+</div>
-                </Draggable>
+                <h4 style={{ marginBottom: '10px' }}>Box details</h4>
+                <p style={{ display: 'flex', flexDirection: 'column' }}>
+                    <span>Cell 1</span>
+                    <span>
+                        height: {initialState.x} width: {initialState.y}
+                    </span>
+                </p>
+                <p style={{ display: 'flex', flexDirection: 'column' }}>
+                    <span>Cell 2</span>
+                    <span>
+                        height: {Math.abs(width - initialState.x)} width: {initialState.y}
+                    </span>
+                </p>
+                <p style={{ display: 'flex', flexDirection: 'column' }}>
+                    <span>Cell 3</span>
+                    <span>
+                        height: {initialState.x} width: {Math.abs(initialState.y - height)}
+                    </span>
+                </p>
+                <p style={{ display: 'flex', flexDirection: 'column' }}>
+                    <span>Cell 4</span>
+                    <span>
+                        height: {Math.abs(initialState.x - width)} width: {Math.abs(initialState.y - height)}
+                    </span>
+                </p>
             </div>
-        </div>
+        </>
     );
 }
 
